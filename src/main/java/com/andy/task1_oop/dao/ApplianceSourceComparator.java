64b719b50bbd76abf7adc3f7fb33ac877c634ea7
OSmol <Olga_Smolyakova@outlook.com>
2018-01-31 13:55:35 +0300

    Update ApplianceSourceComparator.java

--- src/main/java/com/andy/task1_oop/dao/ApplianceSourceComparator.java
+++ src/main/java/com/andy/task1_oop/dao/ApplianceSourceComparator.java
@@ -3,5 +3,6 @@ package main.java.com.andy.task1_oop.dao;
 public class ApplianceSourceComparator {
     public boolean match(Object applianceParamName, Object applianceParamValue, String fileLine){
         return fileLine.contains(String.valueOf(applianceParamName) + "=" + String.valueOf(applianceParamValue));
+        // и вот ради одной этой строки, где, кстати, лучше использовать StringBuilder, ты потом каждый раз создаешь новый объект? МОжет один и тот же постараться использовать?
     }
 }
